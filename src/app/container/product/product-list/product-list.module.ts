201de5283dc22374a4330352227ee8a37fb612ca
phituan <tuandpd00617@fpt.edu.vn>
2019-11-11 08:54:09 +0700

    first commit

--- src/app/container/product/product-list/product-list.module.ts
+++ src/app/container/product/product-list/product-list.module.ts
@@ -1,12 +1,19 @@
+<<<<<<< HEAD
 import {NgModule, OnInit} from '@angular/core';
+=======
+import { NgModule } from '@angular/core';
+>>>>>>> 9473f1566a4ea197c082421032b284aa5cd6790c
 import { CommonModule } from '@angular/common';
 import { ProductListComponent } from './product-list.component';
 import { Routes, Router, RouterModule } from '@angular/router';
 import { TopNavComponent } from '../../layout/top-nav/top-nav.component';
 import { GiftService } from 'src/app/service/gift.service';
 import { GiftCardComponent } from 'src/app/component/gift/gift-card/gift-card.component';
+<<<<<<< HEAD
 import {Gift, GiftResponse} from '../../../model/gift';
 import {ApiResult} from '../../../model/api-results';
+=======
+>>>>>>> 9473f1566a4ea197c082421032b284aa5cd6790c
 export const routes: Routes = [
   {
     path: '', component: ProductListComponent,
@@ -29,6 +36,10 @@ export const routes: Routes = [
     GiftService
   ]
 })
+<<<<<<< HEAD
 export class ProductListModule {
   // tslint:disable-next-line:contextual-lifecycle
 }
+=======
+export class ProductListModule { }
+>>>>>>> 9473f1566a4ea197c082421032b284aa5cd6790c
